ac1c37f52d844afb70cb14c4254c481d41686b7c
Richard Wasonga <rowwasonga@gmail.com>
2025-06-16 19:53:17 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,31 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_list = [p.to_dict() for p in Plant.query.all()]
+        return make_response(plants_list, 200)
+
+    def post(self):
+        data = request.get_json()
+        new_record = Plant(name=data['name'], image=data['image'], price=data['price'],)
+        db.session.add(new_record)
+        db.session.commit()
+
+        plant_dict = new_record.to_dict()
+        response = make_response(plant_dict, 201)
+        return response
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(plant.to_dict(), 200)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')       
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
